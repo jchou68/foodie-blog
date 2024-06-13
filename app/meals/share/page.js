ef35f07543549d270beab0d0efe7e941d0e18b51
jchou68 <jchou6888@gmail.com>
2024-06-13 14:40:00 -0600

    Added functionality to save Share Meal form into local dadatbase

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,6 @@
 import ImagePicker from '@/components/meals/image-picker';
+import { shareMeal } from '@/lib/actions';
+
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -12,7 +14,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form}>
+                <form className={classes.form} action={shareMeal}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -50,7 +52,7 @@ export default function ShareMealPage() {
                             required
                         ></textarea>
                     </p>
-                    <ImagePicker />
+                    <ImagePicker label="Your Image" name="image" />
                     <p className={classes.actions}>
                         <button type="submit">Share Meal</button>
                     </p>
